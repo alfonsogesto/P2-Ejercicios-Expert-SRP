987b06f0e0518c534b6e4734822f1262a4fb0c0b
Alfonso Gesto <alfonso.gesto@correo.ucu.edu.uy>
2023-04-17 02:18:15 -0300

    Ej terminado

--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -8,14 +8,13 @@ namespace Program
         static void Main(string[] args)
         {
             Patient patient1 = new Patient("Leandro", "5876235","27", "09354004", "Casa Central");
-            Doctor doctor1 = new Doctor ("Dr.Albert","C#");
+            Doctor doctor1 = new Doctor ("Dr.Albert","Cardio");
             string appointmentResult = AppointmentService.CreateAppointment(patient1, DateTime.Now, doctor1);
             Console.WriteLine(appointmentResult);
             Patient patient2= new Patient("Romina ", "1843201","20", "093213187", "Policlinica");
-            Doctor doctor2 = new Doctor ("Dr.Maurice","Adios");
+            Doctor doctor2 = new Doctor ("Dr.Maurice","Cardio");
             string appointmentResult2 = AppointmentService.CreateAppointment(patient2, DateTime.Now, doctor2);
             Console.WriteLine(appointmentResult2);
-
            }
         }
     }
